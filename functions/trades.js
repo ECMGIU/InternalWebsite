184ce4231777bd625e05df519ff6144f75fceda6
Wade Fletcher <wbfletch@gmail.com>
2021-01-11 22:59:18 -0500

    add portfolio function

--- functions/trades.js
+++ functions/trades.js
@@ -5,9 +5,34 @@ const tradesRef = db.collection("trades");
 
 exports.ingestion = functions.https.onRequest((request, response) => {
   // TODO: Trade Ingestion Function
+  // See logic in src/pages/TradesPage.jsx
+  // eventually that will be abstracted here
 });
 
-exports.portfolio = functions.https.onRequest((request, response) => {
-  const trades = tradesRef.get();
-  return response.status(200).send(trades);
+exports.portfolio = functions.https.onRequest(async (request, response) => {
+  response.set({ "Access-Control-Allow-Origin": "*" });
+
+  const trades = await tradesRef.get();
+  const portfolio = {};
+
+  trades.docs.forEach((tr) => {
+    // convert document to object
+    const trade = tr.data();
+    const { ticker } = trade;
+
+    if (ticker in portfolio) {
+      // If it exists, add this value
+      portfolio[ticker] += parseFloat(trade.quantity);
+    } else {
+      // If it doesn't, create it with this value
+      portfolio[ticker] = parseFloat(trade.quantity);
+    }
+
+    // If a portfolio item has a 0 or null quantity, remove it
+    if (!portfolio[ticker] && portfolio[ticker] !== undefined) {
+      delete portfolio[ticker];
+    }
+  });
+
+  return response.status(200).send(portfolio);
 });
