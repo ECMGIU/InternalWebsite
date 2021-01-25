039f9d342327fc30924aa3a2127812d18c7fbf82
Wade Fletcher <wbfletch@gmail.com>
2021-01-24 20:36:04 -0500

    break chat into two components and add channels

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,54 +1,21 @@
 // https://github.com/fireship-io/react-firebase-chat/blob/master/src/App.js
 
-import firebase from 'firebase';
+import ChannelPicker from 'components/chat/ChannelPicker';
+import MessageStream from 'components/chat/MessageStream';
 import Sidebar from 'layouts/Sidebar';
-import { auth, firestore } from 'lib/firebase';
-import React, { useRef, useState } from 'react';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import React, { useState } from 'react';
 
 const ChatPage = () => {
-  const scrollRef = useRef();
-
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt');
-
-  const [formValue, setFormValue] = useState('');
-  const [messages] = useCollectionData(query, { idField: 'id' });
-
-  const sendMessage = async (e) => {
-    e.preventDefault();
-
-    await messagesRef.add({
-      text: formValue,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      user: auth.currentUser.email.split('@')[0], // TODO: ensure this still works after transition to IU Auth
-    });
-
-    scrollRef.current.scrollIntoView({ behavior: 'smooth' });
-    setFormValue('');
-  };
+  const [channel, setChannel] = useState('main');
 
   return (
     <Sidebar>
-      <h1 className="title">Chat</h1>
       <div className="flex flex-col w-2/3 h-full">
-        <div className="flex-1 overflow-y-scroll chat-scroll">
-          {messages
-            && messages.map((m) => (
-              <div className="flex space-x-2">
-                <div className="font-bold">{m.user}</div>
-                <div className="flex-1">{m.text}</div>
-                <div className="text-gray-500">{m.createdAt.toDate().toLocaleTimeString()}</div>
-              </div>
-            ))}
-          <span ref={scrollRef} />
+        <h1 className="title">Chat</h1>
+        <ChannelPicker active={channel} callback={setChannel} />
+        <div className="flex-1 mt-4">
+          <MessageStream channel={channel} />
         </div>
-        <form onSubmit={sendMessage} className="flex pt-2">
-          <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="message" className="flex-1 px-1 border border-black focus:outline-none" />
-          <button type="submit" disabled={!formValue} className="text-white bg-black button">
-            Send
-          </button>
-        </form>
       </div>
     </Sidebar>
   );
