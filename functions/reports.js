44de3a3428909fb911edd9bad7a7628ded66cfba
sugandhi <sugandhi@iu.edu>
2021-01-24 23:02:16 -0600

    busboy parsing

--- functions/reports.js
+++ functions/reports.js
@@ -1,6 +1,37 @@
 const functions = require("firebase-functions");
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+const Busboy = require("busboy");
+const { finished } = require("stream");
 
 exports.ingestion = functions.https.onRequest((request, response) => {
-  // TODO: Report Ingestion Function
-  // https://www.sentinelstand.com/article/guide-to-firebase-storage-download-urls-tokens
+  if (req.method !== "POST") {
+    // Return a "method not allowed" error
+    return res.status(405).end();
+  }
+
+  const busboy = new Busboy({ headers: req.headers });
+  const tmpdir = os.tmpdir();
+
+  const fields = {};
+  const uploads = {};
+
+  busboy.on("field", (fieldname, val) => {
+    console.log(`Processed field ${fieldname}: ${val}.`);
+    fields[fieldname] = val;
+  });
+  const fileWrites = [];
+
+  busboy.on("file", (fieldname, file, filename) => {
+    file.on("data", (data) => {
+      uploads["file"] = data;
+    });
+    file.on("end", () => {
+      uploads["filename"] = filename;
+    });
+  });
+  busboy.on("finish", () => {
+    
+  })
 });
