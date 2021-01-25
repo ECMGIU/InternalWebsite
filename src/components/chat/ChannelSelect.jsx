c80536639b4dbc7b3d9dfa624379769890d4037b
Wade Fletcher <wbfletch@gmail.com>
2021-01-25 00:54:41 -0500

    refactor chat components

--- src/components/chat/ChannelSelect.jsx
+++ src/components/chat/ChannelSelect.jsx
@@ -1,10 +1,10 @@
-import CreateChannel from 'components/chat/CreateChannel';
+import ChannelCreate from 'components/chat/ChannelCreate';
 import { firestore } from 'lib/firebase';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-const ChannelPicker = ({ active, callback }) => {
+const ChannelSelect = ({ active, onSelect }) => {
   const channelsRef = firestore.collection('channels');
   const query = channelsRef.orderBy('name');
 
@@ -14,16 +14,16 @@ const ChannelPicker = ({ active, callback }) => {
     <div className="flex items-center px-2 py-1 border border-black space-x-4">
       <div className="font-bold">Channels</div>
       {channels && channels.map((c) => (
-        <button type="button" className={active === c.name && 'font-bold'} onClick={(e) => callback(c.name)}>#{c.name}</button>
+        <button type="button" className={active === c.name && 'font-bold'} onClick={(e) => onSelect(c.name)}>#{c.name}</button>
       ))}
-      <CreateChannel />
+      <ChannelCreate />
     </div>
   );
 };
 
-ChannelPicker.propTypes = {
+ChannelSelect.propTypes = {
   active: PropTypes.string.isRequired,
-  callback: PropTypes.func.isRequired,
+  onSelect: PropTypes.func.isRequired,
 };
 
-export default ChannelPicker;
+export default ChannelSelect;
