2c4c9345a664300e8d74ae235deca6163264a7a2
Wade Fletcher <wbfletch@gmail.com>
2021-01-24 22:53:02 -0500

    added channel creation

---
+++ src/components/chat/CreateChannel.jsx
@@ -0,0 +1,43 @@
+import { firestore } from 'lib/firebase';
+import React, { useState } from 'react';
+import AutosizeInput from 'react-input-autosize';
+
+const CreateChannel = () => {
+  const channelsRef = firestore.collection('channels');
+
+  const [channelName, setChannelName] = useState('');
+  const [editing, setEditing] = useState(false);
+
+  const createChannel = async (e) => {
+    e.preventDefault();
+
+    await channelsRef.add({
+      id: channelName,
+      name: channelName,
+    });
+
+    setEditing(false);
+    setChannelName('');
+  };
+
+  return (
+    <div className="flex items-center">
+      {editing ? (
+        <form onSubmit={createChannel}>
+          <AutosizeInput
+            name="channelName"
+            value={channelName}
+            onChange={(e) => { setChannelName(e.target.value); }}
+            placeholder="channel name"
+          />
+          <button type="submit">+</button>
+        </form>
+      ) : (
+        <button type="button" onClick={(e) => setEditing(true)}>+</button>
+      )}
+
+    </div>
+  );
+};
+
+export default CreateChannel;
