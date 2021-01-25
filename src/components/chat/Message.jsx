b541cf21b78334ff41345ea877a7376483e20f74
Wade Fletcher <wbfletch@gmail.com>
2021-01-25 10:55:51 -0500

    refactor Message component

--- src/components/chat/Message.jsx
+++ src/components/chat/Message.jsx
@@ -2,21 +2,18 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Linkify from 'react-linkify';
 
-const Message = ({ message }) => (
-  <div key={message.id} className="flex space-x-2">
-    <div className="font-bold">{message.user}</div>
-    <div className="flex-1"><Linkify>{message.text}</Linkify></div>
-    {message.createdAt && (<div className="w-40 text-right text-gray-500">{message.createdAt.toDate().toLocaleTimeString()}</div>)}
+const Message = ({ author, text, createdAt }) => (
+  <div className="flex space-x-2">
+    <div className="font-bold">{author}</div>
+    <div className="flex-1"><Linkify>{text}</Linkify></div>
+    {createdAt && (<div className="w-40 text-right text-gray-500">{createdAt.toDate().toLocaleTimeString()}</div>)}
   </div>
 );
 
 Message.propTypes = {
-  message: PropTypes.shape({
-    id: PropTypes.string,
-    user: PropTypes.string,
-    text: PropTypes.string,
-    createdAt: Date,
-  }).isRequired,
+  author: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  createdAt: Date.isRequired,
 };
 
 export default Message;
