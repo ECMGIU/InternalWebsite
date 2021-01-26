2ee41f27d8a7b21316e28e9dd1d2623dc7bf763c
Wade Fletcher <wbfletch@gmail.com>
2021-01-25 22:36:26 -0500

    functional portfolio generation and viewing

--- functions/trades.js
+++ functions/trades.js
@@ -1,7 +1,10 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
 const { db } = require("./shared");
 
 const tradesRef = db.collection("trades");
+const portfoliosRef = db.collection("portfolios");
 
 exports.ingestion = functions.https.onRequest((request, response) => {
   // TODO: Trade Ingestion Function
@@ -9,9 +12,32 @@ exports.ingestion = functions.https.onRequest((request, response) => {
   // eventually that will be abstracted here
 });
 
-exports.portfolio = functions.https.onRequest(async (request, response) => {
-  const trades = await tradesRef.get();
-  const portfolio = {};
+exports.portfolio = functions.https.onCall(async (request, response) => {
+  let trades;
+  let portfolio;
+
+  // Get the most recently created portfolio
+  const portfolios = await portfoliosRef
+      .orderBy("createdAt", "desc")
+      .limit(1)
+      .get();
+
+  // If a portfolio exists,
+  if (!portfolios.empty) {
+    // that's the starting point for our new portfolio,
+    portfolio = portfolios.docs[0].data().contents;
+
+    // and we only want trades since we generated it.
+    trades = await tradesRef.where(
+        "createdAt", ">", portfolios.docs[0].data().createdAt,
+    ).get();
+    // This creates the important caveat that if we upload trades that aren't
+    // chronologically sequential, we'll need to rebuild from scratch.
+  } else {
+    // Otherwise, start with an empty portfolio and all the trades.
+    portfolio = {};
+    trades = await tradesRef.get();
+  }
 
   trades.docs.forEach((tr) => {
     // convert document to object
@@ -32,5 +58,10 @@ exports.portfolio = functions.https.onRequest(async (request, response) => {
     }
   });
 
-  return response.status(200).send(portfolio);
+  // Create a document in the portfolios collection showing the portfolio at
+  // this point in time.
+  await portfoliosRef.add({
+    createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    contents: portfolio,
+  });
 });
