35ed24a52cb2d17daa2bd06ff6e233ecfcf16ad5
Wade Fletcher <wbfletch@gmail.com>
2021-03-02 19:44:41 -0500

    basic current holdings component

--- src/components/dashboard/CurrentHoldings.jsx
+++ src/components/dashboard/CurrentHoldings.jsx
@@ -1,15 +1,29 @@
-import { functions } from 'lib/firebase';
-import React, { useState } from 'react';
+import CurrentHoldingsRow from 'components/dashboard/CurrentHoldingsRow';
+import { firestore } from 'lib/firebase';
+import React from 'react';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 const CurrentHoldings = () => {
-  const [portfolio, setPortfolio] = useState(null);
+  const portfoliosRef = firestore.collection('portfolios');
+  const query = portfoliosRef.orderBy('createdAt', 'desc').limit(1);
+  const [portfolio, loading] = useCollectionData(query, { idField: 'id' });
+  const portfolioContents = portfolio ? portfolio[0].contents : {};
 
-  const portfolioFunction = functions.httpsCallable('trades-portfolio');
-  portfolioFunction()
-    .then((result) => setPortfolio(result.data.output))
-    .catch((error) => { console.error(`error: ${JSON.stringify(error)}`); });
-
-  return <div>{portfolio}</div>;
+  return (
+    <div className="p-3 border border-black">
+      {loading && <p>Loading...</p>}
+      {portfolio
+              && (
+                <table className="table-auto">
+                  {
+                    Object.keys(portfolioContents)
+                      .filter((x) => x[0] !== '-' && portfolioContents[x] > 0)
+                      .map((x) => <CurrentHoldingsRow ticker={x} quantity={portfolioContents[x]} />)
+                  }
+                </table>
+              )}
+    </div>
+  );
 };
 
 export default CurrentHoldings;
