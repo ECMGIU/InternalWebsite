ada7d3d7d91c6cd990cd3947332b232dc378bc6b
Wade Fletcher <wbfletch@gmail.com>
2021-03-28 16:49:25 -0400

    removed credentials

--- functions/trades.js
+++ functions/trades.js
@@ -7,10 +7,12 @@ const { db } = require("./shared");
 const tradesRef = db.collection("trades");
 const portfoliosRef = db.collection("portfolios");
 
-const accountNumber = "5WV99548";
+const tastyworksConfig = functions.config().tastyworks;
+
+const accountNumber = tastyworksConfig.username;
 const credentials = {
-  username: "skeoleia@iu.edu",
-  password: "envisioncapital",
+  username: tastyworksConfig.username,
+  password: tastyworksConfig.password,
 };
 
 const initTastyWorks = async () => {
@@ -26,10 +28,10 @@ const initTastyWorks = async () => {
 
 const toTastyWorksDateFormat = (date) => date.toISOString().split("T")[0];
 
-const parseAction = (action) => {
+const parseAction = (action, type, subtype) => {
   if (action.includes("BOUGHT") || action.includes("Buy")) {
     return "buy";
-  } if (action.includes("DIVIDEND RECEIVED")) {
+  } if (action.includes("DIVIDEND RECEIVED") || subtype.includes("Dividend")) {
     return "dividend received";
   } if (action.includes("REINVESTMENT")) {
     return "reinvestment";
@@ -68,6 +70,9 @@ exports.ingestion = functions.https.onRequest(async (request, response) => {
 
   // pull historical data from tastyworks
   let trades = await TastyWorks.history(accountNumber, start, end);
+  /* return response.status(200).send(
+      `<pre>${JSON.stringify(trades, null, 2)}</pre>`,
+  );*/
 
   // filter out trades without a symbol
   trades = trades.filter((tr) => tr.symbol !== undefined);
@@ -76,7 +81,11 @@ exports.ingestion = functions.https.onRequest(async (request, response) => {
   trades = trades.map((tr) => ({
     date: admin.firestore.Timestamp.fromDate(new Date(tr["executed-at"])),
     ticker: tr.symbol.toUpperCase(),
-    action: tr.action ? parseAction(tr.action) : "unknown",
+    action: parseAction(
+        tr.action ? tr.action : "",
+        tr["transaction-type"] ? tr["transaction-type"] : "",
+        tr["transaction-sub-type"] ? tr["transaction-sub-type"] : "",
+    ),
     commission: parseFloat(tr.commission),
     quantity: parseFloat(tr.quantity),
     price: parseFloat(tr.price),
@@ -95,3 +104,56 @@ exports.ingestion = functions.https.onRequest(async (request, response) => {
 });
 
 
+exports.portfolio = functions.https.onCall(async (request, response) => {
+  let trades;
+  let portfolio;
+
+  // Get the most recently created portfolio
+  const portfolios = await portfoliosRef
+      .orderBy("createdAt", "desc")
+      .limit(1)
+      .get();
+
+  // If a portfolio exists,
+  if (!portfolios.empty) {
+    // that's the starting point for our new portfolio,
+    portfolio = portfolios.docs[0].data().contents;
+
+    // and we only want trades since we generated it.
+    trades = await tradesRef.where(
+        "createdAt", ">", portfolios.docs[0].data().createdAt,
+    ).get();
+    // This creates the important caveat that if we upload trades that aren't
+    // chronologically sequential, we'll need to rebuild from scratch.
+  } else {
+    // Otherwise, start with an empty portfolio and all the trades.
+    portfolio = {};
+    trades = await tradesRef.get();
+  }
+
+  trades.docs.forEach((tr) => {
+    // convert document to object
+    const trade = tr.data();
+    const { ticker } = trade;
+
+    if (ticker in portfolio) {
+      // If it exists, add this value
+      portfolio[ticker] += parseFloat(trade.quantity);
+    } else {
+      // If it doesn't, create it with this value
+      portfolio[ticker] = parseFloat(trade.quantity);
+    }
+
+    // If a portfolio item has a 0 or null quantity, remove it
+    if (!portfolio[ticker] && portfolio[ticker] !== undefined) {
+      delete portfolio[ticker];
+    }
+  });
+
+  // Create a document in the portfolios collection showing the portfolio at
+  // this point in time.
+  await portfoliosRef.add({
+    createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    contents: portfolio,
+  });
+});
