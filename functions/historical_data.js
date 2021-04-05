ffa851aef8d239fc660522e210f32e7b53572d89
wadefletch <>
2021-04-05 14:47:21 +0000

    more hd work

--- functions/historical_data.js
+++ functions/historical_data.js
@@ -1,32 +1,21 @@
 const functions = require("firebase-functions");
-
+const yahooFinance = require('yahoo-finance');
 const axios = require("axios");
 
 const { db } = require("./shared");
 
-exports.ingestion = functions.https.onRequest((request, response) => {
+exports.ingestion = functions.https.onRequest(async (request, response) => {
   const { ticker, startDate, endDate } = request.query;
 
   const historicalDataRef = db.collection("historical_data");
-
   const tickerHistoricalDataRef = historicalDataRef.doc(ticker).collection("data");
 
-  axios.get(`https://autoc.finance.yahoo.com/autoc?lang=en&query=${ticker}`)
-      .then((res) => {
+  const data = await yahooFinance.historical({
+    symbol: ticker,
+    from: startDate,
+    to: endDate,
+    period: 'd'
+  })
 
-        const result = res.data.ResultSet.Result[0];
-
-        
-        if (result && result.symbol === ticker.toUpperCase()) {
-          
-          tickerRef.set({date: result.date, Open: result.Open, Low: result.Low, High: result.High, Close: result.Close, Volume: result.Volume}, 
-            {merge: true,});
-
-          response.status(200).send("Done.");
-        } else {
-          response.status(404).send(`Not Found: ${ticker}`);
-        }
-      }).catch((error) => {
-        response.status(500).send(`Error: ${error.message}`);
-      });
+  return response.status(200).send(data)
 });
