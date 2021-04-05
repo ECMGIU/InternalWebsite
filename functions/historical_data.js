8eb773027c2090a8799ed1e52fdb2349fdfa2e53
Sandeep Amarnath <samarna@iu.edu>
2021-04-05 10:13:52 -0400

    partial code for the ingestion function

--- functions/historical_data.js
+++ functions/historical_data.js
@@ -1,5 +1,32 @@
 const functions = require("firebase-functions");
 
+const axios = require("axios");
+
+const { db } = require("./shared");
+
 exports.ingestion = functions.https.onRequest((request, response) => {
-  // TODO: Historical Data Ingestion Function
+  const { ticker, startDate, endDate } = request.query;
+
+  const historicalDataRef = db.collection("historical_data");
+
+  const tickerHistoricalDataRef = historicalDataRef.doc(ticker).collection("data");
+
+  axios.get(`https://autoc.finance.yahoo.com/autoc?lang=en&query=${ticker}`)
+      .then((res) => {
+
+        const result = res.data.ResultSet.Result[0];
+
+        
+        if (result && result.symbol === ticker.toUpperCase()) {
+          
+          tickerRef.set({date: result.date, Open: result.Open, Low: result.Low, High: result.High, Close: result.Close, Volume: result.Volume}, 
+            {merge: true,});
+
+          response.status(200).send("Done.");
+        } else {
+          response.status(404).send(`Not Found: ${ticker}`);
+        }
+      }).catch((error) => {
+        response.status(500).send(`Error: ${error.message}`);
+      });
 });
