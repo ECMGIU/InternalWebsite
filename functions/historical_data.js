1e4c1302c7e55c8fc9e537ce0ec2b7a676c78fc5
Wade Fletcher <wbfletch@gmail.com>
2021-04-05 11:30:05 -0400

    historical data function

--- functions/historical_data.js
+++ functions/historical_data.js
@@ -1,21 +1,46 @@
 const functions = require("firebase-functions");
-const yahooFinance = require('yahoo-finance');
-const axios = require("axios");
+const yahooFinance = require("yahoo-finance");
 
 const { db } = require("./shared");
+
+const tickersRef = db.collection("tickers");
+
 
 exports.ingestion = functions.https.onRequest(async (request, response) => {
   const { ticker, startDate, endDate } = request.query;
 
-  const historicalDataRef = db.collection("historical_data");
-  const tickerHistoricalDataRef = historicalDataRef.doc(ticker).collection("data");
-
-  const data = await yahooFinance.historical({
+  let data = await yahooFinance.historical({
     symbol: ticker,
     from: startDate,
     to: endDate,
-    period: 'd'
-  })
+    period: "d",
+  });
 
-  return response.status(200).send(data)
+  if (!data) {
+    return response.status(404);
+  }
+
+  data = data.map((d) => {
+    const doc = {
+      date: new Date(d.date),
+      ...d,
+    };
+    delete doc.symbol;
+    return doc;
+  });
+
+  const batch = db.batch();
+
+  data.forEach((d) => {
+    const { date, ...doc } = d;
+    const ref = tickersRef
+        .doc(ticker)
+        .collection("historical-data")
+        .doc(date.toISOString().slice(0, 9));
+    batch.set(ref, doc);
+  });
+
+  batch.commit();
+
+  return response.status(200);
 });
